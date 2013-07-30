558269a1581dd6eb644cb6967c9550664ca7e8f2
Muhammad Hafiz <ibnukamy@gmail.com>
2013-07-30 13:08:30 +0800

    Add to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mebay::Application.config.secret_token = 'ebbf9f4bcaed31f269782d8ce68187ea2f2475999c9b1ed7f51a5183919945ee7116625fa2b0fc47737d76341d657aa4720193e08523a56c021b2c983d2a0b35'
